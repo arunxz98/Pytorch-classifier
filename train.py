a8fb836c07b856f0334a5901f8ea828ad57506bc
Arun Simharajula <arunsimharajula98@gmail.com>
2024-06-09 21:25:59 +0100

    ready for trial 1 and also added gitignore

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ import json
 
 # import mlflow 
 import mlflow
-from mlflow_utils import get_mlflow_experiment
+# from mlflow_utils import get_mlflow_experiment
 
 
 from torch.utils.tensorboard import SummaryWriter
@@ -29,7 +29,7 @@ from torchvision.transforms.functional import InterpolationMode
 from base_model import MobNetv2_custom_classes
 
 
-experiment = get_mlflow_experiment(experiment= "PyTorchClassifier")
+experiment = mlflow.get_experiment_by_name("PyTorchClassifier")
 
 cudnn.benchmark = True
 # plt.ion()   # interactive mode
@@ -71,83 +71,84 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs):
     
-    
-    since = time.time()
+    with mlflow.start_run(run_name="experiment_trial1", experiment_id=experiment.experiment_id) as run:
+        mlflow.pytorch.autolog()
+        since = time.time()
 
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
+        best_model_wts = copy.deepcopy(model.state_dict())
+        best_acc = 0.0
 
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+        for epoch in range(num_epochs):
+            print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+            print('-' * 10)
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()   # Set model to evaluate mode
 
-            running_loss = 0.0
-            running_corrects = 0
+                running_loss = 0.0
+                running_corrects = 0
 
-            # Iterate over data.
-            for inputs, labels in dataloaders[phase]:
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+                # Iterate over data.
+                for inputs, labels in dataloaders[phase]:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
 
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
 
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        # calculate gradients dy/dx
-                        loss.backward()
-                        # update weights
-                        optimizer.step()
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            # calculate gradients dy/dx
+                            loss.backward()
+                            # update weights
+                            optimizer.step()
 
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-            if phase == 'train':
-                scheduler.step()
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                if phase == 'train':
+                    scheduler.step()
 
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-            
-            # writing logs
-            if phase == 'train':
-                writer.add_scalar("Loss/train",epoch_loss,epoch)
-                writer.add_scalar("Accuracy/train",epoch_acc,epoch)
-            if phase == "val":
-                writer.add_scalar("Loss/val",epoch_loss,epoch)
-                writer.add_scalar("Accuracy/val",epoch_acc,epoch)
-
-
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                    phase, epoch_loss, epoch_acc))
+                
+                # writing logs
+                if phase == 'train':
+                    writer.add_scalar("Loss/train",epoch_loss,epoch)
+                    writer.add_scalar("Accuracy/train",epoch_acc,epoch)
+                if phase == "val":
+                    writer.add_scalar("Loss/val",epoch_loss,epoch)
+                    writer.add_scalar("Accuracy/val",epoch_acc,epoch)
 
 
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+                # deep copy the model
+                if phase == 'val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    best_model_wts = copy.deepcopy(model.state_dict())
 
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model
+
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print('Best val Acc: {:4f}'.format(best_acc))
+
+        # load best model weights
+        model.load_state_dict(best_model_wts)
+        return model
 
 
 
